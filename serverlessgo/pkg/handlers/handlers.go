dc6dc8ba63edda1759918adbb3297b658fa12d81
ikikika <iki.jito@gmail.com>
2022-02-21 12:09:30 +0800

    delete user function in handler

--- serverlessgo/pkg/handlers/handlers.go
+++ serverlessgo/pkg/handlers/handlers.go
@@ -64,7 +64,13 @@ func UpdateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient 
 func DeleteUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (
 	*events.APIGatewayProxyResponse, error
 ) {
-
+	err := user.DeleteUser(req, tableName, dynaClient)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error()),
+		})
+	}
+	return apiResponse(http.StatusOK, result)
 }
 
 func UnhandledMethod() (*events.APIGatewayProxyResponse, error) {
