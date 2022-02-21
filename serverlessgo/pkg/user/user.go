77e4653c67ca032f032cf2e282502fa95cf440eb
ikikika <iki.jito@gmail.com>
2022-02-21 12:34:14 +0800

    update users function in controller

--- serverlessgo/pkg/user/user.go
+++ serverlessgo/pkg/user/user.go
@@ -10,6 +10,7 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+	"github.com/ikikika/golang/serverlessgo/pkg/validators"	
 )
 
 var (
@@ -21,7 +22,7 @@ var (
 	ErrorCouldNotDeleteItem = "could not delete item"
 	ErrorCouldNotDynamoPutItem = "could not dynamo put item"
 	ErrorUserAlreadyExist = "user.User already exist"
-	ErrorUserFDoesNotExist = "user.User does not exist"
+	ErrorUserDoesNotExist = "user.User does not exist"
 )
 
 type User struct {
@@ -108,7 +109,35 @@ func CreateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient 
 	return &u, nil
 }
 
-func UpdateUser() {
+func UpdateUser( req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI ) (
+	*User,
+	error
+){
+	var u User
+	if err := json.Unmarshal( []byte(req.Body), &u ); err != nil {
+		return nil, errors.New(ErrorInvalidEmail)
+	}
+
+	currentUser, _ := FetchUser(u.Email, tableName, dynaClient)
+	if currentUser != nil && len(currentUser.Email) == 0 {
+		return nil, errors.New(ErrorUserDoesNotExist)
+	}
+
+	av, err := dynamodbattribute.MarshalMap(u)
+	if err != nil {
+		return nil, errors.New(ErrorCouldNotMarshalItem)
+	}
+
+	input := & dynamodb.PutItemInput{
+		Item: av,
+		TableName: aws.String(tableName)
+	}
+
+	_, err := dynaClient.PutItem(input)
+	if err != nil {
+		return nil, errors.New(ErrorCouldNotDynamoPutItem)
+	}
+	return &u, nil
 
 }
 
