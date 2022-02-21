4e7c931f5ccd823c4316ffdd47c4d0f61b7c9848
ikikika <iki.jito@gmail.com>
2022-02-21 12:25:28 +0800

    create users in controller

--- serverlessgo/pkg/user/user.go
+++ serverlessgo/pkg/user/user.go
@@ -14,6 +14,14 @@ import (
 
 var (
 	ErrorFailedToFetchRecord = "failed to fetch record"
+	ErrorInvalidUserData = "invalid user data"
+	ErrorFailedtoUnmarshalRecord = "failed to unmarshal record"
+	ErrorInvalidEmail = "invalid email"
+	ErrorCouldNotMarshalItem = "could not marshal item"
+	ErrorCouldNotDeleteItem = "could not delete item"
+	ErrorCouldNotDynamoPutItem = "could not dynamo put item"
+	ErrorUserAlreadyExist = "user.User already exist"
+	ErrorUserFDoesNotExist = "user.User does not exist"
 )
 
 type User struct {
@@ -40,7 +48,7 @@ func FetchUser(email, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*
 	item := new(User)
 	err = dynamodbattribute.UnmarshalMap(result.Item, item)
 	if err != nil {
-		return nil, errors.New(ErrorFailedtoUnmarchalRecord)
+		return nil, errors.New(ErrorFailedtoUnmarshalRecord)
 	}
 	return item, nil
 }
@@ -61,12 +69,43 @@ func FetchUsers(tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*[]User
 
 }
 
-func FetchUsers() {
+func CreateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI)(
+	*User,
+	error
+) {
+	var u User 
 
-}
+	json.Unmarshal( []byte(req.body), &u ); err != nil {
+		return nil, errors.New(ErrorInvalidUserData)
+	}
 
-func CreateUser() {
+	if !validators.IsEmailValid( u.Email ){
+		return nil, errors.New(ErrorInvalidEmail)
+	}
 
+	// check if user already exist
+	currentUser, _ := FetchUser(u.Email, tableName, dynaClient)
+	if currentUser != nil && len(currentUser.Email) != 0 {
+		return nil, errors.New(ErrorUserAlreadyExist)
+	}
+
+	// save user (if user does not exist)
+	av, err := dynamodbattribute.marshalMap(u)
+
+	if err != nil {
+		return nil, errors.New(ErrorCouldNotMarshalItem)
+	}
+
+	input := &dynamodb.PutItemInput{
+		itemL av,
+		TableName: aws.String(tableName)
+	}
+
+	_, err = dynaClient.PutItem.(input)
+	if err != nil {
+		return nil, errors.New(ErrorCouldNotDynamoPutItem)
+	}
+	return &u, nil
 }
 
 func UpdateUser() {
